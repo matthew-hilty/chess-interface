b56dd860953e36c2b41362c9a647dcd5d4d50fa3
MatthewHilty <hilty.matthew@gmail.com>
2015-02-16 11:05:20 -0500

    Initial commit.

---
+++ README.md
@@ -0,0 +1,14 @@
+A chess user-interface for Firefox written in CoughSyrup, a shallow fork of CoffeeScript.
+
+Its runtime dependencies comprise jQuery, jQuery UI, React, and RequireJS.
+Its testing dependencies comprise Jasmine and Testem.
+
+To install this library,
+- `git clone https://github.com/matthew-hilty/chess-interface.git`
+- `npm install`
+
+To build and then launch into Firefox on a UNIX-compatible system,
+- `npm run open`
+
+To run tests,
+- `npm test`
---
+++ test/spec-runner.js
@@ -0,0 +1,54 @@
+require.config({
+  baseUrl : "../src/",
+  paths : {
+    jasmine       : '../test/vendor/jasmine',
+    jasmineHtml   : '../test/vendor/jasmine-html',
+    jFns          : '../test/spec/jasmine-fns',
+    spec          : '../test/spec',
+    specConstants : '../test/spec/spec-constants',
+    testem        : '/testem',
+  },
+  shim : {
+    jasmine : {
+      exports : 'jasmine'
+    },
+    jasmineHtml  : {
+      deps    : ['jasmine'],
+      exports : 'jasmine'
+    }
+  }
+});
+
+require (['jasmineHtml', 'testem'], function () {
+
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var htmlReporter = new jasmine.HtmlReporter();
+
+  jasmineEnv.addReporter(htmlReporter);
+
+  jasmineEnv.specFilter = function (spec) {
+    return htmlReporter.specFilter(spec);
+  };
+
+  var specs = [
+    'spec/assertions-spec',
+    'spec/extractors-spec',
+    'spec/fen-service-spec',
+    'spec/game-service-spec',
+    'spec/king-spec',
+    'spec/knight-spec',
+    'spec/pawn-spec',
+    'spec/piece-spec',
+    'spec/rook-spec',
+    'spec/slider-spec',
+    'spec/stepper-spec',
+    'spec/types-spec',
+    'spec/utilities-spec'
+  ]
+
+	require (specs, function () {
+		jasmineEnv.execute ();
+	});
+});
